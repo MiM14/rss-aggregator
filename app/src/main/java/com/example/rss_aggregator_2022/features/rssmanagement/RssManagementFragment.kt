172a837ebb88f53b3fafdb8a82764b7c1cbebac4
Ismael Martín Moakil <85709107+MiM14@users.noreply.github.com>
2022-11-18 18:32:02 +0100

    views finished

--- app/src/main/java/com/example/rss_aggregator_2022/features/rssmanagement/RssManagementFragment.kt
+++ app/src/main/java/com/example/rss_aggregator_2022/features/rssmanagement/RssManagementFragment.kt
@@ -1,6 +1,21 @@
 package com.example.rss_aggregator_2022.features.rssmanagement
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.rss_aggregator_2022.databinding.FragmentRssManagementBinding
 
 class RssManagementFragment : Fragment() {
+    var binding: FragmentRssManagementBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRssManagementBinding.inflate(inflater)
+        return binding?.root
+    }
 }
