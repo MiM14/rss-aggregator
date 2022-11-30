8ecc604208d39b757144ccfe2cdcbd8563b6a6e0
MiM14 <imxym14@gmail.com>
2022-11-30 16:01:38 +0100

    Revert "get use case as a suspend function for either"
    This reverts commit 7a4607206bd48cc3a64f7a4773eca6c4352db160.

--- app/src/main/java/com/example/rss_aggregator_2022/features/domain/GetSourceRssUseCase.kt
+++ app/src/main/java/com/example/rss_aggregator_2022/features/domain/GetSourceRssUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.rss_aggregator_2022.features.domain
 
 class GetSourceRssUseCase(private val repository: RssRepository) {
-    suspend fun execute(): List<Rss> =
+    fun execute(): List<Rss> =
         repository.getSourceRss()
 }
