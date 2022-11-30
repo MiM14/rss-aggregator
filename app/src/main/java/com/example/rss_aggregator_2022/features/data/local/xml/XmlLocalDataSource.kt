3227890e4989893bdeecef1f1ac60221fc0a26f3
MiM14 <imxym14@gmail.com>
2022-11-30 18:31:57 +0100

    fix bugs

--- app/src/main/java/com/example/rss_aggregator_2022/features/data/local/xml/XmlLocalDataSource.kt
+++ app/src/main/java/com/example/rss_aggregator_2022/features/data/local/xml/XmlLocalDataSource.kt
@@ -4,13 +4,12 @@ import android.content.SharedPreferences
 import com.example.rss_aggregator_2022.app.commons.KSerializer
 import com.example.rss_aggregator_2022.features.data.local.LocalDataSource
 import com.example.rss_aggregator_2022.features.domain.Rss
-import com.google.gson.Gson
 
 class XmlLocalDataSource(private val sharedPref: SharedPreferences, private val kSerializer: KSerializer): LocalDataSource {
     private val editor = sharedPref.edit()
 
     override fun saveRss(name: String, urlRss: String) {
-        editor.putString(urlRss, kSerializer.toJson(Rss(name,urlRss),Rss::class.java))
+        editor.putString(name, kSerializer.toJson(Rss(name,urlRss),Rss::class.java))
         editor.apply()
     }
 }
