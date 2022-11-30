e77e308a4c3697e3f59d9492560df7ebe1a5629e
MiM14 <imxym14@gmail.com>
2022-11-30 15:57:50 +0100

    rssFeed getter on data

--- app/src/main/java/com/example/rss_aggregator_2022/features/data/local/LocalDataSource.kt
+++ app/src/main/java/com/example/rss_aggregator_2022/features/data/local/LocalDataSource.kt
@@ -1,5 +1,8 @@
 package com.example.rss_aggregator_2022.features.data.local
 
+import com.example.rss_aggregator_2022.features.domain.Rss
+
 interface LocalDataSource {
     fun saveRss(name:String,urlRss:String)
+    fun getUserRssList():List<Rss>
 }
--- app/src/main/java/com/example/rss_aggregator_2022/features/data/local/xml/XmlLocalDataSource.kt
+++ app/src/main/java/com/example/rss_aggregator_2022/features/data/local/xml/XmlLocalDataSource.kt
@@ -4,7 +4,6 @@ import android.content.SharedPreferences
 import com.example.rss_aggregator_2022.app.commons.KSerializer
 import com.example.rss_aggregator_2022.features.data.local.LocalDataSource
 import com.example.rss_aggregator_2022.features.domain.Rss
-import com.google.gson.Gson
 
 class XmlLocalDataSource(private val sharedPref: SharedPreferences, private val kSerializer: KSerializer): LocalDataSource {
     private val editor = sharedPref.edit()
@@ -12,5 +11,13 @@ class XmlLocalDataSource(private val sharedPref: SharedPreferences, private val 
     override fun saveRss(name: String, urlRss: String) {
         editor.putString(urlRss, kSerializer.toJson(Rss(name,urlRss),Rss::class.java))
         editor.apply()
+    }
+
+    override fun getUserRssList(): List<Rss> {
+        val rssFeed = mutableListOf<Rss>()
+        sharedPref.all.map{
+            rssFeed.add(kSerializer.fromJson(it.value as String, Rss::class.java))
+        }
+        return rssFeed
     }
 }
