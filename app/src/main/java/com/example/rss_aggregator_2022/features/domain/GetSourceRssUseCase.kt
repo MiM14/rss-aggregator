e91b34b3fa0e234eb5056c16b532ef7d58da677e
MiM14 <imxym14@gmail.com>
2022-11-30 16:02:54 +0100

    fixed bug

--- app/src/main/java/com/example/rss_aggregator_2022/features/domain/GetSourceRssUseCase.kt
+++ app/src/main/java/com/example/rss_aggregator_2022/features/domain/GetSourceRssUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.rss_aggregator_2022.features.domain
 
-class GetSourceRssUseCase(private val repository: RssRepository) {
+class GetSourceRssUseCase(private val repository: RssRepository){
     fun execute(): List<Rss> =
         repository.getSourceRss()
 }
