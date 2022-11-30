c84c5c90fdc285488ca4c76331dbc4c5949ee23d
MiM14 <imxym14@gmail.com>
2022-11-30 18:15:37 +0100

    issue on progress

---
+++ app/src/main/java/com/example/rss_aggregator_2022/features/presentation/rssmanagement/adapter/RssManagementViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.rss_aggregator_2022.features.presentation.rssmanagement.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+
+class RssManagementViewHolder(itemView: View): ViewHolder(itemView) {
+}
