d18f34329b02248db6b29ff5c59b601b029b0cc2
Ismael Martín Moakil <imxym14@gmail.com>
2022-12-01 09:26:33 +0100

    changes requested

--- app/src/main/java/com/example/rss_aggregator_2022/MainActivity.kt
+++ app/src/main/java/com/example/rss_aggregator_2022/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.example.rss_aggregator_2022.databinding.ActivityMainBinding
+import com.example.rss_aggregator_2022.features.rssfeed.RssFeedFragmentDirections
+import com.example.rss_aggregator_2022.features.rssmanagement.RssManagementFragmentDirections
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -24,11 +26,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpNavigation(){
-        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemReselectedListener {
+        findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
             when(it.itemId){
-                R.id.to_rss_feed_item -> findNavController(R.id.main_fragment_view).navigate(R.id.feed)
-                R.id.to_rss_manager_item-> findNavController(R.id.main_fragment_view).navigate(R.id.management)
-                R.id.to_your_profile_item-> findNavController(R.id.main_fragment_view).navigate(R.id.profile)
+                R.id.to_rss_feed_item -> findNavController(R.id.main_fragment_view).navigate(NavGraphDirections.toFeed())
+                R.id.to_rss_manager_item-> findNavController(R.id.main_fragment_view).navigate(NavGraphDirections.toManagement())
+                R.id.to_your_profile_item-> findNavController(R.id.main_fragment_view).navigate(NavGraphDirections.toProfile())
             }
             true
         }
